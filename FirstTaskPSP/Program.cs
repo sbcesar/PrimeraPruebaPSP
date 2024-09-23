9465419faf4fa07de7c1a85e597e4073a2aa36d7
sbcesar <sbcesar@iesrafaelalberti.es>
2024-09-23 22:17:47 +0200

    Arreglado la lista de objetos, falta coprobar funcionalidad de métodos

--- FirstTaskPSP/Program.cs
+++ FirstTaskPSP/Program.cs
@@ -1,32 +1,40 @@
 ﻿using FirstTaskPSP.classes;
 
-class Program
+Axe hacha = new Axe { Name = "Gran hacha de Caballero Negro", Damage = 20 };
+Sword espada = new Sword { Name = "Uchigatana", Damage = 16 };
+Helmet casco = new Helmet { Name = "Yelmo de Gundyr", Armor = 8 };
+Shield escudo = new Shield { Name = "Gran escudo de Havel", Armor = 12 };
+
+Character guerrero = new Character
 {
-    static void Main(string[] args)
-    {
-        Axe hacha = new Axe { Name = "Gran hacha de CAballero Negro", Damage = 20 };
-        Sword espada = new Sword { Name = "Uchigatana", Damage = 16 };
-        Helmet casco = new Helmet { Name = "Yelmo de Gundyr", Armor = 8 };
-        Shield escudo = new Shield { Name = "Gran escudo de Havel", Armor = 12 };
-    }
+    Name = "Siegmeyer de Catarina",
+    MaxHitPoints = 100,
+    CurrentHitPoints = 100,
+    BaseDamage = 11,
+    BaseArmor = 13
+};
 
-    private Character guerrero = new Character
-    {
-        Name = "Siegmeyer de Catarina",
-        MaxHitPoints = 100,
-        BaseDamage = 11,
-        BaseArmor = 13
-    };
-    
-    
+guerrero.AddItem(hacha);
+guerrero.AddItem(casco);
+guerrero.AddItem(escudo);
 
-    private Character mago = new Character
-    {
-        Name = "Hermana Friede",
-        MaxHitPoints = 85,
-        BaseDamage = 23,
-        BaseArmor = 6
-    };
-    
-    
-}
+Character mago = new Character
+{
+    Name = "Hermana Friede",
+    MaxHitPoints = 85,
+    BaseDamage = 23,
+    BaseArmor = 6
+};
+
+mago.AddItem(espada);
+mago.AddItem(casco);
+mago.AddItem(hacha);
+mago.RemoveItem(hacha);
+
+mago.CurrentHitPoints = guerrero.Attack(mago.MaxHitPoints, guerrero.BaseDamage);
+Console.WriteLine("El guerrero " + guerrero.Name + " ha atacado a " + mago.Name + ".");
+Console.WriteLine(mago.CurrentHitPoints + " hp.");
+int hpHealed = mago.Heal(20);
+Console.WriteLine("El mago " + mago.Name + " se ha curado " + hpHealed + ".");
+Console.WriteLine("La hp actual de " + mago.Name + " es " + mago.CurrentHitPoints + ".");
+
--- FirstTaskPSP/classes/Character.cs
+++ FirstTaskPSP/classes/Character.cs
@@ -11,20 +11,24 @@ public class Character
     public int BaseArmor { get; set; }
     
     private List<IItem> _inventory = new List<IItem>();
+    
+    public List<IItem> Inventory { get => _inventory; set => _inventory = value; }
 
     public void AddItem(IItem item)
     {
-        _inventory.Add(item);
+        Inventory.Add(item);
+        Console.WriteLine("Se ha añadido " + item);
     }
 
     public void RemoveItem(IItem item)
     {
-        _inventory.Remove(item);
+        Inventory.Remove(item);
+        Console.WriteLine("Se ha eliminado " + item);
     }
 
-    public int Attack()
+    public int Attack(int hp, int damage)
     {
-        return BaseDamage;
+        return hp - damage;
     }
 
     public int Defense()
@@ -32,13 +36,16 @@ public class Character
         return BaseArmor;
     }
 
-    public void Heal(int hp)
+    public int Heal(int hp)
     {
-        CurrentHitPoints += hp;
+        int hpHealed = MaxHitPoints - (CurrentHitPoints + hp);
+        CurrentHitPoints = hp;
         if (CurrentHitPoints >= MaxHitPoints)
         {
             CurrentHitPoints = MaxHitPoints;
         }
+
+        return hpHealed;
     }
 
     public void ReceiveDamage(int totalDamage)
